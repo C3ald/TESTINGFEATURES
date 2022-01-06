358910e8646761215c8716b0971de32888188c5e
C3ald <specklesjr10@gmail.com>
2022-01-05 20:43:29 -0700

    started smart contracts

---
+++ smartcontracts/lexar.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+class Lexar():
+	def __init__(self):
+	    pass
+
+	def compile(self, file):
+		""" Compiles the file """
+		pass
+
+	def checkKeywards(self, file):
+		""" reads the file and checks syntax """
